ce28b3a67dda093b92c3e3bcd15775f43d77239a
kaetee425 <kingpang@outlook.com>
2018-01-15 19:12:48 -0800

    config, heroku

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./models/Todo.js')
 // const is ES6 syntax
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/TodoList')
+const MONGODB_URI = keys.mongoURI || 'mongodb://localhost/TodoList'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 
 app.get('/', (req, res) => {
 	res.send({message: 'hello you'});
