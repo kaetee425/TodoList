4d49b699e11b1599808c256cb51ef94f245702e6
kaetee425 <kingpang@outlook.com>
2018-02-05 20:03:23 -0800

    fixing the google secret

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID, 
 	mongoURI: process.env.MONGO_URI,
 	cookieKey: process.env.COOKIE_KEY,
-	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
 
